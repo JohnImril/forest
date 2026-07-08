316150ba0d325a4a5427fcf6f1ec14b7437c4c29
Nikita Maximov <john.maks595@gmail.com>
2026-07-08 23:52:34 +0500

    chore: create svelte app setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite'
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vite.dev/config/
 export default defineConfig({
   base: process.env.GITHUB_ACTIONS ? '/forest/' : '/',
-  plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
+  plugins: [svelte()],
 })
